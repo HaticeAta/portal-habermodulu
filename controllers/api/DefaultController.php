6e7a58c4f923e51af559fc97f63166a72d9ef667
HaticeAta <hatice.ata.98@gmail.com>
2018-12-14 17:23:06 +0300

    first

---
+++ controllers/api/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\habermodulu\controllers\api;
+
+
+/**
+ * Default controller for the `habermodulu` module
+ */
+class DefaultController extends \kouosl\base\controllers\api\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
---
+++ controllers/console/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\habermodulu\controllers\console;
+
+
+/**
+ * Default controller for the `habermodulu` module
+ */
+class DefaultController extends \kouosl\base\controllers\console\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
